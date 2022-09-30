190a02b6f90e33f6fe6b7791a586238a2d884e10
Anton <s214611@dtu.dk>
2022-09-30 16:54:26 +0200

    hej

--- project1_main_.py
+++ project1_main_.py
@@ -1,1 +1,17 @@
-#hej drenge
+import numpy as np
+import pandas as pd
+
+filename = 'Weather Training Data.csv'
+df = pd.read_csv(filename)
+
+df = df.loc[df['Location'] == 'Canberra']
+
+df = df[["RainToday", "MinTemp", "MaxTemp", "Evaporation", "Sunshine", "WindGustSpeed", "Humidity9am", "Pressure9am",
+         "Cloud9am", "Temp9am"]]
+
+print(df)
+
+print(df.isnull().sum())
+
+
+
