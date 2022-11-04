49821c3cfc2f1f67e254581ed94a3ae63b19b479
MathiasDamsgaard <s214647@dtu.dk>
2022-11-04 10:41:59 +0100

    update

--- project2.py
+++ project2.py
@@ -6,11 +6,12 @@ from scipy.linalg import svd
 import seaborn as sns
 from scipy.stats import boxcox
 from project1 import df_trans
-from sklearn import preprocessiong
+from sklearn import preprocessing
 
 #Define target and traning variables (without WindGustSpeed as per conclusion of last report)
 target_reg = df_trans["Rainfall"]
 target_class = df_trans["RainToday"]
 var = df_trans.drop(["WindGustSpeed","Rainfall", "RainToday"], axis=1)
 
+#Standardize data
 var_scaled = preprocessing.scale(var)
